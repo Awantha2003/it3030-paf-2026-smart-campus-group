6bbe223922463983e9335a0e1db8549e9ec519bb
Jithmini Yohansa <jithminiyohansa03@gmail.com>
2026-04-18 09:22:04 +0530

    implement Facility CRUD service and controller

---
+++ Backend/PAF_Smart/src/main/java/com/tech/spcours/paf_smart/controller/FacilityController.java
@@ -0,0 +1,64 @@
+package com.tech.spcours.paf_smart.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tech.spcours.paf_smart.dto.CreateFacilityRequest;
+import com.tech.spcours.paf_smart.dto.FacilityResponse;
+import com.tech.spcours.paf_smart.service.FacilityService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/facilities")
+@RequiredArgsConstructor
+public class FacilityController {
+
+    private final FacilityService facilityService;
+
+    @GetMapping
+    @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
+    public List<FacilityResponse> getAllFacilities() {
+        return facilityService.getAllFacilities();
+    }
+
+    @GetMapping("/{code}")
+    @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
+    public FacilityResponse getFacilityByCode(@PathVariable String code) {
+        return facilityService.getFacilityByCode(code);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    @PreAuthorize("hasRole('ADMIN')")
+    public FacilityResponse createFacility(@Valid @RequestBody CreateFacilityRequest request) {
+        return facilityService.createFacility(request);
+    }
+
+    @PutMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public FacilityResponse updateFacility(
+            @PathVariable String id,
+            @Valid @RequestBody CreateFacilityRequest request) {
+        return facilityService.updateFacility(id, request);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PreAuthorize("hasRole('ADMIN')")
+    public void deleteFacility(@PathVariable String id) {
+        facilityService.deleteFacility(id);
+    }
+}
